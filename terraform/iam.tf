296e50ea8b94b1f78261178e382b90486e5ac446
fuguozhu1 <fuguozhu1994@gmail.com>
2026-09-21 12:34:03 +0800

    test: trigger CodePipeline deployment

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -29,9 +29,32 @@ resource "aws_iam_role_policy_attachment" "ec2_ssm" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
+resource "aws_iam_policy" "codedeploy_s3_read" {
+  name = "aws-devops-codedeploy-s3-read"
+
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Sid    = "CodeDeployArtifactRead"
+        Effect = "Allow"
+
+        Action = [
+          "s3:GetObject",
+          "s3:GetObjectVersion"
+        ]
+
+        Resource = "${aws_s3_bucket.pipeline_artifacts.arn}/*"
+      }
+    ]
+  })
+}
+
 resource "aws_iam_role_policy_attachment" "ec2_codedeploy_s3" {
   role       = aws_iam_role.ec2.name
-  policy_arn = "arn:aws:iam::498623467710:policy/aws-devops-codedeploy-s3-read"
+  policy_arn = aws_iam_policy.codedeploy_s3_read.arn
 }
 
 resource "aws_iam_instance_profile" "ec2" {
